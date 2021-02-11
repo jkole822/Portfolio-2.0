ca2e0ed0e635e37a86a90fd88b8c0f80bf8dba6e
Kole Gasior <jkole822@gmail.com>
2021-02-11 11:22:04 -0500

    card-flip

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,15 @@
 $(document).ready(function () {
 	M.AutoInit();
 
-	$(".grid").isotope({
-		// options
+	// Card Flip Sizing
+	$(".project-image").each(function (index) {
+		const width = $(this).width();
+		const height = $(this).height();
 
+		$(this).parents(".card").width(width).height(height);
+	});
+	// Isotope
+	$(".grid").isotope({
 		itemSelector: ".grid-item",
 		layoutMode: "packery",
 	});
