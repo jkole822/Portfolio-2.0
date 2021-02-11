bb16a632cf140bb3744b6b8fdb444198575be309
Kole Gasior <jkole822@gmail.com>
2021-02-10 21:13:02 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,24 @@
+require("./models/Project");
+const express = require("express");
+const exphbs = require("express-handlebars");
+const mongoose = require("mongoose");
+
+const htmlRoutes = require("./routes/html-routes");
+const keys = require("./config/keys");
+const app = express();
+const port = process.env.PORT || 3000;
+
+mongoose.connect(keys.mongoURI);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.engine("hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
+app.set("view engine", "hbs");
+
+app.use(htmlRoutes);
+
+app.listen(port, () => {
+	console.log(`Server is listening on port ${port}`);
+});
