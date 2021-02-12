da64e8560f877e83275b6900e21ac96c764e9aa7
Kole Gasior <jkole822@gmail.com>
2021-02-11 22:16:29 -0500

    style page

--- public/js/index.js
+++ public/js/index.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
 	M.AutoInit();
 
 	// Card Flip Sizing
-	$(".project-image").each(function (index) {
+	$(".project-image").each(function () {
 		const width = $(this).width();
 		const height = $(this).height();
 
@@ -10,8 +10,40 @@ $(document).ready(function () {
 	});
 
 	// Isotope
-	$(".grid").isotope({
+	const $grid = $(".grid").isotope({
 		itemSelector: ".grid-item",
 		layoutMode: "packery",
+	});
+
+	// Isotope Filters
+	$("select").change(function () {
+		const tech = $(this).val();
+		if (tech === "*") {
+			$grid.isotope({ filter: tech });
+		} else {
+			$grid.isotope({ filter: `[data-tech*="${tech}"]` });
+			const height = $("html").height();
+			console.log(height);
+			$("body").height(height);
+			$("#background").height(height);
+		}
+	});
+
+	// LocomotiveScroll
+	const scroller = new LocomotiveScroll({
+		el: document.querySelector("[data-scroll-container]"),
+		smooth: true,
+	});
+
+	const resizeProjectContainer = () => {
+		const windowWidth = $(window).width();
+		const imagesPerRow = Math.floor(windowWidth / 375);
+		$("#my-projects").width(imagesPerRow * 375);
+	};
+
+	resizeProjectContainer();
+
+	$(window).resize(() => {
+		resizeProjectContainer();
 	});
 });
