92bb7128eaf176c9ee0311164b03d9b393c480ea
Kole Gasior <jkole822@gmail.com>
2021-02-21 19:42:16 -0500

    add mobile style settings

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,16 @@
+// Mobile Settings
+if (
+	/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+		navigator.userAgent
+	)
+) {
+	// true for mobile device
+	$(".mobile-center").css("justify-content", "center");
+	$(".header").css("margin-left", "0").css("margin-right", "0");
+	$("#my-name").removeClass("small-space").addClass("big-space");
+	$("#my-role").css("font-size", "1.25rem");
+}
+
 // LocomotiveScroll
 const scroller = new LocomotiveScroll({
 	el: document.querySelector("[data-scroll-container]"),
