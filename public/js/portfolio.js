92ec639c63a1c2091112358afa9cbd411b65c259
Kole Gasior <jkole822@gmail.com>
2021-04-13 19:12:34 -0400

    add align-items to image container, add userAgent response to portfolio page

--- public/js/portfolio.js
+++ public/js/portfolio.js
@@ -1,3 +1,19 @@
+const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+
+if (
+	/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+		navigator.userAgent
+	)
+) {
+	// true for mobile device
+	$(".mobile-center").css("justify-content", "center");
+	$(".header").css("margin-left", "0").css("margin-right", "0");
+	$("#my-name").removeClass("small-space").addClass("big-space");
+	$("#my-role").css("font-size", "1.25rem");
+} else if (isSafari) {
+	M.toast({ html: "Try an alternative browser for optimal viewing" });
+}
+
 let pictureWidth;
 
 const resizePictureWidth = () => {
